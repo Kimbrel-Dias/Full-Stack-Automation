643b29ded0be9ace6afdcb9fc4cefd032ce1ea88
kimbrel dias <diaskimbrel555@gmail.com>
2026-06-24 20:21:14 +0530

    Daily Assignment added

---
+++ Daily Assignments/Assignement1-220626.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class Assignement1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int StudentID = 101;
+		System.out.println(StudentID);
+		
+		char grade = 'A';
+		System.out.println(grade);
+		
+		boolean passed = true;
+		System.out.println(passed);
+		
+		byte age = 18;
+		System.out.println(age);
+
+	}
+
+}
--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 - [X] Assignment 1 - Submitted on 14 June 26  
 - [X] Assignment 2 - Submitted on 14 June 26 [REPOSITORY](https://github.com/Kimbrel-Dias/employee-management)
+
+## Daily Assignments
+
+- [X] JAVA Day1 - Submitted on 24 June 26
